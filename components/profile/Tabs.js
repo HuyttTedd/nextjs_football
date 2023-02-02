5681bde417e9a335b8bb9c6231715194a0345610
James <dungdt41>
2023-02-02 18:12:15 +0700

    code css tabs

--- components/profile/Tabs.js
+++ components/profile/Tabs.js
@@ -1,14 +1,41 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import styles from '../../styles/profile/tabs.module.scss';
+import { useWindowSize } from '../utils/ScreenSize';
+import { Popover, Button, Text } from "@nextui-org/react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSearch,
+  faList,
+  faAnchor,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Tabs() {
+  const size = useWindowSize();
+
   return (
+    size.width > 578 ? 
     <span className={styles['tab-container']}>
-        <a className={styles['test']}>Left</a>
+        <a className={styles['sticky-selected']}>Left</a>
         <a>Middle</a>
         <a>Test1</a>
+        <span>{size.width}px / {size.height}px</span>
+    </span>
+    :
+    <span className={styles['tab-container']}>
+        <a className={styles['sticky-selected']}>Left</a>
+        <Popover disableAnimation>
+          <Popover.Trigger>
+            <Button animated={false} auto flat color="light">
+              View More 
+              <FontAwesomeIcon
+                icon={faList}
+                style={{ fontSize: 16, color: "black", marginLeft: 10 }}
+              />
+            </Button>
+          </Popover.Trigger>
+          <Popover.Content>
+            <Text css={{ p: "$10" }}>This is the content of the popover. </Text>
+          </Popover.Content>
+        </Popover>
     </span>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
 import "../styles/globals.css";
+import { NextUIProvider } from '@nextui-org/react';
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+
+// Tell Font Awesome to skip adding the CSS automatically 
+// since it's already imported above
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
 
 export default MyApp;
